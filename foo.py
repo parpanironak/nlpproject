7acd4261fd2539b8531a7aab1c0fe2547445a2f9
Julien Rabinow <jnr305@nyu.edu>
2015-04-27 19:53:53 -0400

    Parser works but is VERY slow. Use wikiextractor.py instead Started work on associating tags with relevant corpus

---
+++ foo.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+
+import os, sys, re
+import cPickle as pickle
+
+INPUT_DATA_DIR = "./corpus_data"
+OUTPUT_DATA_DIR = "./foo"
+
+# TODO: write wrapper function: pass in the path to a directory, dictionary is made
+# TODO: convince Ronak that his idea of passing in a single tag to associate_tags is bad, pointless and unoptimized
+
+def extract_sentences(filepath):
+    try:
+        with open(filepath) as myfile:
+            file_contents = myfile.readlines()
+    except IOError, e:
+        sys.stderr.write(e + '\n')
+        exit(1)
+    file_contents = "".join(file_contents)
+    # TODO: properly segment into phrases. Use nltk?
+    sentences = file_contents.split(".?!")
+    return sentences
+ 
+
+# TODO: debug me
+# TODO: properly process tags: get rid of brackets and 
+# TODO: store file offsets in memory as opposed to entire phrase (save some mem).
+#	Provide associated wrapper functions
+# TODO: discuss previous TODO. Is it strictly necessary? Sure it's more optimized,
+#	but it comes at the cost of a lot of extra work, and the resulting
+#	optimizations may not be worth it
+def associate_tags(tags, sentences):
+    dico = {}
+    for sentence in sentences:
+        for tag in tags:
+            if tag in sentence:
+                dico[tag] = dico.get(tag, []).append(sentence)
+    return dico
+
+
+def print_dict_to_files(mydict):
+    if not os.path.isdir(OUTPUT_DATA_DIR):
+        if os.path.exists(OUTPUT_DATA_DIR):
+            sys.stderr.write("Error: file exists\n")
+            exit(1)
+        else:
+            os.makedirs(OUTPUT_DATA_DIR)
+            for disambiguated_form in mydict.keys():
+                with open(os.path.join(OUTPUT_DATA_DIR, disambiguated_form), "w") as f:
+                    pickle.dump(mydict[disambiguated_form], f)
+
+
+def main():
+    sentences = []
+    for arg in sys.argv[1:]:
+        sentences.extend(extract_sentences(arg))
+    print associate_tags(["english", "Coming"], sentences) 
+
+if __name__ == "__main__":
+    main()
--- parser.py
+++ parser.py
@@ -1,15 +1,56 @@
 #!/usr/bin/env python2
 
-import sys
+import sys, re
 try:
     from bs4 import BeautifulSoup
 except ImportError:
-	sys.stderr.write("{0} depends on python {1} module. Run 'pip install {1}' from a shell.\n".format(sys.argv[0], "beautifulsoup"))
-	exit(1)
+    sys.stderr.write("{0} depends on python {1} module. Run 'pip install {1}' from a shell.\n".format(sys.argv[0], "BeautifulSoup"))
+    exit(1)
+
+try:
+    import urllib2
+except ImportError:
+    sys.stderr.write("{0} depends on python {1} module. Run 'pip install {1}' from a shell.\n".format(sys.argv[0], "urllib"))
+    exit(1)
+
+def http_download(url):
+    # max file size 100MB
+    maxFileSize = 1024*1024*100
+    try:
+        request = urllib2.urlopen(url)
+        data = request.read(maxFileSize)
+        # if file too large
+        if request.read(1) != '':
+            return None
+        else:
+            return data
+    except urllib2.HTTPError, e:
+        print("urllib2.HttpError")
+        exit()
+    except urllib2.URLError, e:
+        print("urllib2.URLError")
+        exit()
+
+
+def extract_tags(text):
+    tag_regex = re.compile("\[\[([^:\[\]]*:?)*\]\]")
+    return [ str(tag).strip("[]") for tag in
+        tag_regex.findall(text) if not str(tag).strip("[]").startswith(":")]
+
 
 def main():
-    print "Hello World!"
+    dico = {}
+    for arg in sys.argv[1:]:
+        try:
+            with open(arg) as myfile:
+                soup = BeautifulSoup(myfile)
+                for tag in extract_tags(soup.text):
+                    dico[tag] = True
+                for tag in dico.keys():
+                    print tag
+        except IOError, e:
+            sys.stderr.write("{0}\n".format(e))
 
 
 if __name__ == "__main__":
-        main()
+    main()
