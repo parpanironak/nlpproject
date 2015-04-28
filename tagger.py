81e2a5b420ee33f28f7a69edfdec909a357db28a
Julien Rabinow <jnr305@nyu.edu>
2015-04-28 03:51:55 -0400

    Cleaned up code

--- tagger.py
+++ tagger.py
@@ -4,18 +4,27 @@ import os, sys, re
 import cPickle as pickle
 
 INPUT_DATA_DIR = "./corpus_data"
-OUTPUT_DATA_DIR = "./foo"
+OUTPUT_DATA_DIR = "./tagger_data"
 
 # TODO: write wrapper function: pass in the path to a directory, dictionary is made
 # TODO: convince Ronak that his idea of passing in a single tag to associate_tags is bad, pointless and unoptimized
+
+class Tag:
+    def __init__(self, tagname, surface_forms = [], phrases = []):
+        self.tagname = tagname
+        self.surface_forms = surface_forms
+        self.phrases = phrases
+    def append(self, string):
+        self.phrases.append(string)
+
 
 def extract_sentences(filepath):
     try:
         with open(filepath) as myfile:
             file_contents = myfile.readlines()
     except IOError, e:
-        sys.stderr.write(e + '\n')
-        exit(1)
+        sys.stderr.write(str(e) + '\n')
+        return []
     file_contents = "".join(file_contents)
     # TODO: properly segment into phrases. Use nltk?
     sentences = file_contents.split(".?!")
@@ -32,22 +41,31 @@ def extract_sentences(filepath):
 def associate_tags(tags, sentences):
     dico = {}
     for sentence in sentences:
+        print sentence
         for tag in tags:
             if tag in sentence:
-                dico[tag] = dico.get(tag, []).append(sentence)
+                dico[tag] = dico.get(tag, Tag(tag)).append(sentence)
     return dico
 
 
-def print_dict_to_files(mydict):
-    if not os.path.isdir(OUTPUT_DATA_DIR):
-        if os.path.exists(OUTPUT_DATA_DIR):
+def print_dict_to_files(mydict, out_dir = OUTPUT_DATA_DIR):
+    if not os.path.isdir(out_dir):
+        if os.path.exists(out_dir):
             sys.stderr.write("Error: file exists\n")
             exit(1)
         else:
-            os.makedirs(OUTPUT_DATA_DIR)
+            try:
+                os.makedirs(out_dir)
+            except OSError:
+                sys.stderr.write("Error creating directory\n")
+                exit(1)
     for disambiguated_form in mydict.keys():
-        with open(os.path.join(OUTPUT_DATA_DIR, disambiguated_form), "w") as f:
-            pickle.dump(mydict[disambiguated_form], f)
+        try:
+            with open(os.path.join(out_dir, disambiguated_form), "w") as f:
+                pickle.dump(mydict[disambiguated_form], f)
+        except IOError, e:
+            sys.stderr.write("Error creating file\n")
+            exit(1)
 
 
 def main():
