b73626354a8208eb5e5cec1304dbed88b60e1233
Julien Rabinow <jnr305@nyu.edu>
2015-04-30 18:17:42 -0400

    Finished API

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Dump file will be have form
 .....content.....
 </page>
 
-need to store each xml tag in a separate file with name <title>.xml (title is the entity in the entity tag pair)
+need to store each xml tag in a separate file with name TITLE.xml (TITLE is the entity in the entity tag pair)
 
 need to go through each of the file to check if it contains one of the key words and make a list of the file paths and store in a file (grep command exits/ can make separate list for each word)
 
@@ -42,3 +42,11 @@ while we go through the files we also need to keep a count of each Tag-Entity pa
 need to go through the entire corpus to determine the idf / need to go through the reduced corpus to find inverse entity frequency
 
 when running system use the reduced corpus and the idf values to create a vector and store it in memory and comapare to the vector of the generated for the input document 
+
+-----------------------
+Code conventions:
+Each level of indentation is 4 spaces
+Variable names must obey the following regex: [a-z_][a-zA-Z0-9_]* No camlCasing
+Function names must obey the following regex: [a-z_][a-zA-Z0-9_]* No camlCasing
+Class names must obey the following regex: [A-Z][a-zA-Z0-9_]* with camlCasing
+File names must obey the following regex: [a-z_][a-zA-Z0-9_]* with camlCasing
--- tagger.py
+++ tagger.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python2
 
 import os, sys, re
-import cPickle as pickle
 
 INPUT_DATA_DIR = "./corpus_data"
 OUTPUT_DATA_DIR = "./tagger_data"
 
-# TODO: write wrapper function: pass in the path to a directory, dictionary is made
-# TODO: convince Ronak that his idea of passing in a single tag to associate_tags is bad, pointless and unoptimized
 
-class Tag:
-    def __init__(self, tagname, surface_forms = [], phrases = []):
-        self.tagname = tagname
-        self.surface_forms = surface_forms
-        self.phrases = phrases
+class Entity:
+    def __init__(self, entity_name, count = 0, sentences = []):
+        self.entity_name = entity_name
+        self.count = count
+        self.sentences = sentences
     def append(self, string):
-        self.phrases.append(string)
+        self.sentences.append(string)
+    def increment(value = 1):
+        self.count += value
+
+
+def create_dict(taglist, corpus_path = INPUT_DATA_DIR):
+    dico = {}
+    testfunc = lambda x: map(lambda filepath: sys.stdout.write("{0}\n".format(os.path.join(x[0], filepath))), x[2])
+    map(testfunc, os.walk(corpus_path))
+    return dico
+
+
+def extract_sentences_with_links(tag, filepath):
+    sentences = extract_sentences(filepath)
+    return associate_tags([tag], sentences)
+
+# compatibility with Ronak's existing code
+extractSentencesWithLinks = extract_sentences_with_links
 
 
 def extract_sentences(filepath):
@@ -40,15 +54,15 @@ def extract_sentences(filepath):
 #    optimizations may not be worth it
 def associate_tags(tags, sentences):
     dico = {}
-    for sentence in sentences:
-        print sentence
-        for tag in tags:
+    for tag in tags:
+        for sentence in sentences:
+            print sentence
             if tag in sentence:
-                dico[tag] = dico.get(tag, Tag(tag)).append(sentence)
+                dico[tag] = dico.get(tag, Entity(tag)).append(sentence)
     return dico
 
 
-def print_dict_to_files(mydict, out_dir = OUTPUT_DATA_DIR):
+def print_dict_to_files(mydict, out_dir = OUTPUT_DATA_DIR, use_pickle = False):
     if not os.path.isdir(out_dir):
         if os.path.exists(out_dir):
             sys.stderr.write("Error: file exists\n")
@@ -59,20 +73,24 @@ def print_dict_to_files(mydict, out_dir = OUTPUT_DATA_DIR):
             except OSError:
                 sys.stderr.write("Error creating directory\n")
                 exit(1)
-    for disambiguated_form in mydict.keys():
-        try:
-            with open(os.path.join(out_dir, disambiguated_form), "w") as f:
-                pickle.dump(mydict[disambiguated_form], f)
-        except IOError, e:
-            sys.stderr.write("Error creating file\n")
-            exit(1)
+    if use_pickle:
+        import cPickle as pickle
+        for disambiguated_form in mydict.keys():
+            try:
+                with open(os.path.join(out_dir, disambiguated_form), "w") as f:
+                    pickle.dump(mydict[disambiguated_form], f)
+            except IOError, e:
+                sys.stderr.write("Error creating file\n")
+                exit(1)
 
 
 def main():
     sentences = []
-    for arg in sys.argv[1:]:
-        sentences.extend(extract_sentences(arg))
-    print associate_tags(["english", "Coming"], sentences) 
+    create_dict([], sys.argv[1])
+    #for arg in sys.argv[1:]:
+    #    sentences.extend(extract_sentences(arg))
+    #print associate_tags(["english", "Coming"], sentences) 
+
 
 if __name__ == "__main__":
     main()
