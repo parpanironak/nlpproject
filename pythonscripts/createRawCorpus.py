b8f4daeefe6ee2ff576df2c4ac0de0e12cf1c557
Julien Rabinow <jnr305@nyu.edu>
2015-04-30 23:57:33 -0400

    Optimized createVector.py, cleaned up pythonscripts code

--- pythonscripts/createRawCorpus.py
+++ pythonscripts/createRawCorpus.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
  
 import sys, getopt
 import xml.etree.cElementTree as ET
--- pythonscripts/createVectors.py
+++ pythonscripts/createVectors.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 import sys, getopt
 import xml.etree.cElementTree as ET
@@ -13,36 +13,25 @@ def removePunctuations(s):
     return s.translate(table, string.punctuation)
 
 def createCountMap(wordList):
-	hmap = {};
-	if not isinstance(word , list)
-		return None
-		ema
-	for word in wordList:
-		if hamp.has_key(word):
-			hmap[word] = hmap[word] + 1
-		else:
-			hmap[word] = 1
-			
-	return hmap
+    hmap = {};
+    if not isinstance(word , list):
+        return None
+        ema
+    for word in wordList:
+        hmap[word] = hmap.get(word, 0) + 1
+    return hmap
 
 def methodd(tag, entity, odir):
-	
-	termFrequencyVector = {}
-	pattern = r"(?i)\[\[%s \\| %s\]\]" % entity, tag
-	entityFilePath = odir + "/corpus/" + tag + "/" + entity + ".txt"
-	if(os.path.isfile(entityFilePath)):	
-		wordList = []	
-		with open(entityFilePath) as entityFile:
-			entityFileLine = entityFile.readline()
-			if entityFileLine == "<doc>" or entityFileLine == "</doc>" :
-				
-				tempList = re.split(pattern, entityFileLine)
-				for temp in tempList:
-					partialWordList = removePunctuations(temp.strip()).split()
-					wordList = wordList + [x.lower() for x in partialWordList]
-		
-		
-		
-	 
-	
-	
+    termFrequencyVector = {}
+    pattern = re.compile(r"(?i)\[\[{0} \\| {1}\]\]".format(entity, tag))
+    entityFilePath = odir + "/corpus/" + tag + "/" + entity + ".txt"
+
+    if(os.path.isfile(entityFilePath)):
+        wordList = []
+        with open(entityFilePath) as entityFile:
+            entityFileLine = entityFile.readline()
+            if entityFileLine == "<doc>" or entityFileLine == "</doc>" :
+                tempList = pattern.split(entityFileLine)
+                for temp in tempList:
+                    partialWordList = removePunctuations(temp.strip()).split()
+                    wordList = wordList + [x.lower() for x in partialWordList]
